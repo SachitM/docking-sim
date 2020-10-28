8e15f07776d2363c532b8c54e744804160e12b89
SachitM <sachitmahajan.sm@gmail.com>
2020-10-28 04:27:52 -0400

    Ported Goal Publisher to Autoware simulation, added lift

---
+++ src/align_navigation/src/utils.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+import math
+
+import rospy
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Point, PoseArray
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
+
+num_waypoints = 3
+waypoint_tol = 0.1
+retrace_waypoint_tol = 0.15
+
+wheelbase = 1.9
+ODOM_INF = "/ground_truth/state"
