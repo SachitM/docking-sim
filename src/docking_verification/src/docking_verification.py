e261cb95279cb650e07bedecd1b1508a03b403c8
sanil <sanil.pande@gmail.com>
2020-11-07 20:47:46 -0500

    added state listener

--- src/docking_verification/src/docking_verification.py
+++ src/docking_verification/src/docking_verification.py
@@ -1,66 +1,65 @@
 #!/usr/bin/env python
+"""Calculates offset of chassis (lidar) from pod center."""
+
 import rospy
-from sensor_msgs.msg import PointCloud2
-import sensor_msgs.point_cloud2 as pc2
 import numpy as np
+
+from sensor_msgs import point_cloud2 as pc2
+from sensor_msgs.msg import PointCloud2
 from std_msgs.msg import Float64
+from StateMachine.msg import StateOut
 
-moving_average = np.zeros(5)
-counter = 0
 
-def docking_offset():
-    pub = rospy.Publisher('dock_offset', Float64, queue_size=10)
-    # rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        # hello_str = "hello world %s" % rospy.get_time()
-        # rospy.loginfo(hello_str)
-        pub.publish(round(np.mean(moving_average), 4))
-        rate.sleep()
+class DockingVerification():
+    """Check state, publish docking offset from pod."""
+    def __init__(self, average_len=10):
+        self.average_len = average_len
+        self.moving_average = np.zeros(average_len)
+        self.counter = 0
+        self.docking_state = False
 
-def velodyne_points_callback(point_cloud):
-    global counter
-    x = []
-    y = []
-    z = []
-    points = np.zeros(3)
-    for p in pc2.read_points(point_cloud, field_names = ("x", "y", "z"), skip_nans=True):
-        # print (" x : %f  y: %f  z: %f" %(p[0],p[1],p[2]))
-        if abs(p[0]) < 15 and abs(p[1]) < 15 and abs(p[2]) < 0.05:
-            # points = np.append(points, np.array([p[0], p[1], p[2]]), axis = 0)
-            x.append(p[0])
-            y.append(p[1])
-            z.append(p[2])
+        self.publisher = rospy.Publisher('dock_offset', Float64, queue_size=10)
+        self.lidar_sub = rospy.Subscriber('system_status', StateOut,
+                                          self.state_listener)
+        self.lidar_sub = rospy.Subscriber("velodyne_points", PointCloud2,
+                                          self.velodyne_points_callback)
 
-    points = np.zeros((3, len(x)))
-    points[0,:] = np.array(x)
-    points[1,:] = np.array(y)
-    points[1,:] = np.array(z)
+    def velodyne_points_callback(self, point_cloud):
+        """Estimate and publish pod center relative to lidar."""
+        if self.docking_state is False: # no need to calculate
+            return
 
-    # print(len(points[0]))
+        x = y = z = []
+        for p in pc2.read_points(point_cloud, field_names=("x", "y", "z"), skip_nans=True):
+            if abs(p[0]) < 15 and abs(p[1]) < 15 and abs(p[2]) < 0.05:
+                x.append(p[0])
+                y.append(p[1])
+                # z.append(p[2])
 
-    mean_x = np.mean(points[0])
-    mean_y = np.mean(points[1])
-    offset = np.sqrt(mean_x**2 + mean_y**2)
+        points = np.zeros((3, len(x)))
+        points[0, :] = np.array(x)
+        points[1, :] = np.array(y)
+        # points[1, :] = np.array(z)
 
-    moving_average[counter%5] = offset
-    counter += 1
-    # print("Offset ", mean_x, mean_y)
-    # moving_average.append(offset)
-    # print("MA = ", round(sum(moving_average) / len(moving_average), 3 ) )
-    # print("MA = ", round(np.mean(moving_average), 4))
+        mean_x = np.mean(points[0])
+        mean_y = np.mean(points[1])
+        offset = np.sqrt(mean_x**2 + mean_y**2)
 
-def listener():
+        self.moving_average[self.counter % self.average_len] = offset
+        self.counter += 1
 
-    return None
+        #lidar is assumed to be at chassis center
+        self.publisher.publish(round(np.mean(self.moving_average), 4))
+
+    def state_listener(self, state):
+        """Listen to chassis state and update own state."""
+        if state.DestState == 4:    #TODO: Add in more states?
+            self.docking_state = True
+        else:
+            self.docking_state = False
+
 
 if __name__ == '__main__':
-
     rospy.init_node('docking_verification', anonymous=True)
-
-    flag_useval = 0
-    rospy.Subscriber("velodyne_points", PointCloud2, velodyne_points_callback)
-    docking_offset()
-
+    dv = DockingVerification()
     rospy.spin()
-    listener()
